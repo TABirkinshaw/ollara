06b31de0ef0910e43c7e3f8899ecebe9887e76e2
Tristan Birkinshaw <tbirkinshaw@hotmail.com>
2018-06-19 12:22:25 +1000

    Initialisation of the project

---
+++ Ollara/Ollara/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ollara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
