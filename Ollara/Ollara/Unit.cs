a900bc7621d802830d74400875e4343788eece6d
Tristan Birkinshaw <tbirkinshaw@hotmail.com>
2018-06-19 17:00:46 +1000

    Added currently empty Unit class

---
+++ Ollara/Ollara/Unit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ollara
+{
+    class Unit
+    {
+        public Unit(String newName)
+        {
+            
+        }
+    }
+}
