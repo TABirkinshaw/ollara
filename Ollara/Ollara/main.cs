01ee37bca9209f93472d82ce5f00d62a414562c3
Tristan Birkinshaw <tbirkinshaw@hotmail.com>
2018-06-19 12:41:13 +1000

    Updated auto-generated 'Program.cs' to 'main.cs'

--- Ollara/Ollara/main.cs
+++ Ollara/Ollara/main.cs
@@ -2,11 +2,13 @@
 
 namespace Ollara
 {
-    class Program
+    class main
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
