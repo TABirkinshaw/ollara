b7cd502878e10e80beed2a5645be2dc836dbea4a
Tristan Birkinshaw <tbirkinshaw@hotmail.com>
2018-07-02 17:06:28 +1000

    Added member variables and methods to BaseUnit.cs and Character.cs

--- Ollara/Ollara/BaseUnit.cs
+++ Ollara/Ollara/BaseUnit.cs
@@ -4,11 +4,20 @@ using System.Text;
 
 namespace Ollara
 {
-    class BaseUnit
+    abstract class BaseUnit
     {
-        public BaseUnit(String name)
-        {
-            
-        }
+        protected String name;
+        protected Race race;
+        protected float health;
+        protected float energy;
+        protected float luck;
+        protected float panic;
+
+        public abstract String Name { get; }
+        public abstract Race Race { get; }
+        public abstract float Health { get; set; }
+        public abstract float Energy { get; set; }
+        public abstract float Luck { get; set; }
+        public abstract float Panic { get; set; }
     }
 }
--- Ollara/Ollara/Character.cs
+++ Ollara/Ollara/Character.cs
@@ -6,9 +6,52 @@ namespace Ollara
 {
     class Character : BaseUnit
     {
-        public Character(String name, Background background)
+        public Character(String name, Race race, Background background)
         {
 
+        }
+
+        private Background background;
+        private float might;
+        private float finesse;
+        private float astra;
+        private float agility;
+        private float toughness;
+        private float acuity;
+        private float empathy;
+        private float contempt;
+
+        public override String Name
+        {
+            get { return Name; }
+        }
+
+        public override Race Race
+        {
+            get { return Race; }
+        }
+
+        public override float Health
+        {
+            get { return Health; }
+            set { Health = value; }
+        }
+
+        public override float Energy
+        {
+            get { return Energy; }
+            set { Energy = value; }
+        }
+
+        public override float Luck
+        {
+            get { return Luck; }
+        }
+
+        public override float Panic
+        {
+            get { return Panic; }
+            set { Panic = value; }
         }
     }
 }
